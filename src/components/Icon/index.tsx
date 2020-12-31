3b289c2ff8caffe936e80fa33f5505547c4446a2
Dan Barcinas <barcinasdan@gmail.com>
2020-12-31 16:08:32 +1000

    Added icon color props

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import {SvgProps} from "react-native-svg"
 import {scale} from "react-native-size-matters"
+import {color, ColorProps, useRestyle} from "@shopify/restyle"
 
 import Chat from "./svg/Chat.svg"
 import Like from "./svg/Like.svg"
@@ -8,15 +9,25 @@ import More from "./svg/More.svg"
 import Next from "./svg/Next.svg"
 import Search from "./svg/Search.svg"
 
+import {Theme} from "../../theme"
+
 type IconName = "chat" | "like" | "more" | "next" | "search"
 
-type Props = {
+type Props = ColorProps<Theme> & {
   name: IconName
   size?: number
 }
 
-function Icon({name, size = 24}: Props) {
-  const props: SvgProps = {width: scale(size), height: scale(size)}
+const restyleFunctions = [color]
+
+function Icon({name, size = 24, ...rest}: Props) {
+  const colorProps = useRestyle(restyleFunctions, rest)
+  const props: SvgProps = {
+    width: scale(size),
+    height: scale(size),
+    ...colorProps
+  }
+
   switch (name) {
     case "chat":
       return <Chat {...props} />
